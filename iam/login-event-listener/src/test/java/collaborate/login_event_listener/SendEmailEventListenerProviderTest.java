161ddacd0362ed8bc8a5e1247d8088cdbecb1f16
Zick <thao.tran@kapten.com>
2020-12-04 14:12:02 +0100

    COL-14 create a function to send welcome email to the new user

--- iam/login-event-listener/src/test/java/collaborate/login_event_listener/SendEmailEventListenerProviderTest.java
+++ iam/login-event-listener/src/test/java/collaborate/login_event_listener/SendEmailEventListenerProviderTest.java
@@ -32,9 +32,13 @@ public class SendEmailEventListenerProviderTest {
     @Mock
     private Event event;
 
+    @Mock
+    private UserProvider userProvider;
+
     private String FAKE_REALM_ID = "fake_id";
     private String IDP_ADMIN_ROLE = System.getenv("IDP_ADMIN_ROLE");
-
+    private String APPLICATION_NAME = System.getenv("APPLICATION_NAME");
+    private String fakeNewUserId = "fake_id";
 
     @Before
     public void beforeEach() {
@@ -42,6 +46,7 @@ public class SendEmailEventListenerProviderTest {
         realmProvider = mock(RealmProvider.class);
         realmModel = mock(RealmModel.class);
         event = mock(Event.class);
+        userProvider = mock(UserProvider.class);
     }
 
     @After
@@ -50,13 +55,20 @@ public class SendEmailEventListenerProviderTest {
         reset(realmProvider);
         reset(realmModel);
         reset(event);
+        reset(userProvider);
     }
     @Test
     public void onEventSetRealmAndWhenEventIsNotRegister() {
-        when(session.realms()).thenReturn(realmProvider);
+        UserModel userModel = mock(UserModel.class);
+
         when(event.getRealmId()).thenReturn(FAKE_REALM_ID);
-        when(realmProvider.getRealm(FAKE_REALM_ID)).thenReturn(realmModel);
         when(event.getType()).thenReturn(EventType.LOGOUT);
+
+        when(realmProvider.getRealm(FAKE_REALM_ID)).thenReturn(realmModel);
+
+        when(session.realms()).thenReturn(realmProvider);
+
+        when(userProvider.getUserById(fakeNewUserId, realmModel)).thenReturn(userModel);
 
         SendEmailEventListenerProvider provider = new SendEmailEventListenerProvider(session);
 
@@ -80,21 +92,32 @@ public class SendEmailEventListenerProviderTest {
 
     @Test
     public void onEventWhenEventIsRegister() {
-        when(session.realms()).thenReturn(realmProvider);
+        UserModel userModel = mock(UserModel.class);
+
         when(event.getRealmId()).thenReturn(FAKE_REALM_ID);
-        when(realmProvider.getRealm(FAKE_REALM_ID)).thenReturn(realmModel);
         when(event.getType()).thenReturn(EventType.REGISTER);
+        when(event.getUserId()).thenReturn(fakeNewUserId);
+
+        when(session.realms()).thenReturn(realmProvider);
+        when(session.users()).thenReturn(userProvider);
+
+        when(realmProvider.getRealm(FAKE_REALM_ID)).thenReturn(realmModel);
+
+        when(userProvider.getUserById(fakeNewUserId, realmModel)).thenReturn(userModel);
 
         SendEmailEventListenerProvider provider = new SendEmailEventListenerProvider(session);
 
-        SendEmailEventListenerProvider spyProvider = spy(provider);
-        doNothing().when(spyProvider).sendNotificationEmailForIDPAdmins(event);
+         SendEmailEventListenerProvider spyProvider = spy(provider);
+         when(spyProvider.getRealm()).thenReturn(realmModel);
+         doNothing().when(spyProvider).sendNotificationEmailForIDPAdmins(userModel);
+         doNothing().when(spyProvider).sendNewAccountCreatedConfirmationEmailToUser(userModel);
 
-        // when call onEvent with event type is Register
-        spyProvider.onEvent(event);
+         // when call onEvent with event type is Register
+         spyProvider.onEvent(event);
 
-        // then
-        verify(spyProvider, times(1)).sendNotificationEmailForIDPAdmins(event);
+         // then
+         verify(spyProvider, times(1)).sendNotificationEmailForIDPAdmins(userModel);
+         verify(spyProvider, times(1)).sendNewAccountCreatedConfirmationEmailToUser(userModel);
     }
 
     @Test
@@ -102,13 +125,10 @@ public class SendEmailEventListenerProviderTest {
         // Mock all objects required for the function
         DefaultEmailSenderProvider emailSenderProvider = mock(DefaultEmailSenderProvider.class);
         KeycloakContext keycloakContext = mock(KeycloakContext.class);
-        UserProvider userProvider = mock(UserProvider.class);
         Map<String,String> fakeSmtpConfig = new HashMap<>();
         RoleModel idpAdminRole = mock(RoleModel.class);
 
         List<UserModel> listUsers = createTestUsers();
-
-        String fakeNewUserId = "fake_id";
 
         UserModel newUser = new TestUserModel(fakeNewUserId, "Toto", "Tata");
 
@@ -137,17 +157,17 @@ public class SendEmailEventListenerProviderTest {
         when(spyProvider.getIdpAdminRoleModel()).thenReturn(idpAdminRole);
 
         // When spy provider call sendNotificationEmailForIDPAdmins method
-        spyProvider.sendNotificationEmailForIDPAdmins(event);
+        spyProvider.sendNotificationEmailForIDPAdmins(newUser);
 
         UserModel firstIdpAdmin = listUsers.get(0);
         UserModel secondIdpAdmin = listUsers.get(3);
 
         String firstExpectedHtml = "<p>Hello,</p>";
-        firstExpectedHtml += "<p><b>Toto Tata</b> has created a new account on " + System.getenv("APPLICATION_NAME") + " PCC Platform.</p>";
+        firstExpectedHtml += "<p><b>Toto Tata</b> has created a new account on " + APPLICATION_NAME + " PCC Platform.</p>";
         firstExpectedHtml += "Please validate this user";
 
         String secondExpectedHtml = "<p>Hello,</p>";
-        secondExpectedHtml += "<p><b>Toto Tata</b> has created a new account on " + System.getenv("APPLICATION_NAME") + " PCC Platform.</p>";
+        secondExpectedHtml += "<p><b>Toto Tata</b> has created a new account on " + APPLICATION_NAME + " PCC Platform.</p>";
         secondExpectedHtml += "Please validate this user";
 
         // Expect the emailSenderProvider is called twice with the following arguments
@@ -157,6 +177,105 @@ public class SendEmailEventListenerProviderTest {
             verify(emailSenderProvider)
                     .send(fakeSmtpConfig, secondIdpAdmin, "New User Created!", "", secondExpectedHtml);
             verifyNoMoreInteractions(emailSenderProvider);
+        } catch (EmailException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void sendNewAccountCreatedConfirmationEmailToUser() {
+        // Mock all objects required for the function
+        DefaultEmailSenderProvider emailSenderProvider = mock(DefaultEmailSenderProvider.class);
+        KeycloakContext keycloakContext = mock(KeycloakContext.class);
+        Map<String,String> fakeSmtpConfig = new HashMap<>();
+
+        UserModel newUser = new TestUserModel(fakeNewUserId, "Toto", "Tata");
+
+        when(session.realms()).thenReturn(realmProvider);
+        when(session.getContext()).thenReturn(keycloakContext);
+        when(session.users()).thenReturn(userProvider);
+
+        when(keycloakContext.getRealm()).thenReturn(realmModel);
+
+        when(realmModel.getSmtpConfig()).thenReturn(fakeSmtpConfig);
+
+        when(userProvider.getUserById(fakeNewUserId, realmModel)).thenReturn(newUser);
+
+        when(event.getRealmId()).thenReturn(FAKE_REALM_ID);
+        when(event.getType()).thenReturn(EventType.REGISTER);
+        when(event.getUserId()).thenReturn(fakeNewUserId);
+
+        // Given the spy SendEmailEventListenerProvider with mock variables
+        SendEmailEventListenerProvider provider = new SendEmailEventListenerProvider(session);
+        SendEmailEventListenerProvider spyProvider = spy(provider);
+        when(spyProvider.getEmailSenderProvider()).thenReturn(emailSenderProvider);
+        when(spyProvider.getRealm()).thenReturn(realmModel);
+
+        testSendEmailToNewUser(newUser, spyProvider, emailSenderProvider, fakeSmtpConfig, "normal");
+        testSendEmailToNewUser(newUser, spyProvider, emailSenderProvider, fakeSmtpConfig, "withoutFirstName");
+        testSendEmailToNewUser(newUser, spyProvider, emailSenderProvider, fakeSmtpConfig, "withoutLastName");
+        testSendEmailToNewUser(newUser, spyProvider, emailSenderProvider, fakeSmtpConfig, "withoutAnyNames");
+
+    }
+
+    /**
+     * Call the sendNewAccountCreatedConfirmationEmailToUser and check if the emailSenderProvider is called with the correct arguments
+     *
+     * @param newUser {Keycloak.UserModel}  - the new user who is just created
+     * @param spyProvider {SendEmailEventListenerProvider}  - the listener that perform the action sendNewAccountCreatedConfirmationEmailToUser
+     * @param emailSenderProvider {Keycloak.DefaultEmailSenderProvider} - the email sender which will send the email
+     * @param fakeSmtpConfig {Map<String, String>}  - the config of smtp
+     * @param scenario {String}   - the scenario that we want to test
+     */
+    private void testSendEmailToNewUser(
+        UserModel newUser,
+        SendEmailEventListenerProvider spyProvider,
+        DefaultEmailSenderProvider emailSenderProvider,
+        Map<String, String> fakeSmtpConfig,
+        String scenario
+    ) {
+        String expectedHtml = "";
+
+        switch (scenario) {
+            case "normal":
+                newUser.setFirstName("Toto");
+                newUser.setLastName("Tata");
+                expectedHtml = "<p>Welcome<b> Toto Tata</b> to <b>" + APPLICATION_NAME + " PCC Platform</b>,</p>";
+                expectedHtml += "<p>Your account has been successfully created. ";
+                expectedHtml += "You will receive a notification email when your <b>" + APPLICATION_NAME + " administrator grants</b> you access.</p>";
+                break;
+            case "withoutFirstName":
+                newUser.setFirstName(null);
+                newUser.setLastName("Tata");
+                expectedHtml = "<p>Welcome<b> Tata</b> to <b>" + APPLICATION_NAME + " PCC Platform</b>,</p>";
+                expectedHtml += "<p>Your account has been successfully created. ";
+                expectedHtml += "You will receive a notification email when your <b>" + APPLICATION_NAME + " administrator grants</b> you access.</p>";
+                break;
+            case "withoutLastName":
+                newUser.setFirstName("Toto");
+                newUser.setLastName(null);
+                expectedHtml = "<p>Welcome<b> Toto</b> to <b>" + APPLICATION_NAME + " PCC Platform</b>,</p>";
+                expectedHtml += "<p>Your account has been successfully created. ";
+                expectedHtml += "You will receive a notification email when your <b>" + APPLICATION_NAME + " administrator grants</b> you access.</p>";
+                break;
+            case "withoutAnyNames":
+                newUser.setFirstName(null);
+                newUser.setLastName(null);
+                expectedHtml = "<p>Welcome<b></b> to <b>" + APPLICATION_NAME + " PCC Platform</b>,</p>";
+                expectedHtml += "<p>Your account has been successfully created. ";
+                expectedHtml += "You will receive a notification email when your <b>" + APPLICATION_NAME + " administrator grants</b> you access.</p>";
+                break;
+            default:
+                break;
+        }
+
+        // When call sendNewAccountCreatedConfirmationEmailToUser
+        spyProvider.sendNewAccountCreatedConfirmationEmailToUser(newUser);
+
+        // Expect the emailSenderProvider is called with the following arguments
+        try {
+            verify(emailSenderProvider, times(1))
+                    .send(fakeSmtpConfig, newUser, "Your account has been created", "", expectedHtml);
         } catch (EmailException e) {
             e.printStackTrace();
         }
