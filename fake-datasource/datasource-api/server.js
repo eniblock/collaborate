686c2664c81e2ca74fe3453889200801430e5e8b
Maxime Vanmeerbeck <maxime@theblockchain-group.com>
2021-01-11 19:59:27 +0100

    download, new json

--- fake-datasource/datasource-api/server.js
+++ fake-datasource/datasource-api/server.js
@@ -6,30 +6,27 @@ const middlewares = jsonServer.defaults()
 const jwt = require('jsonwebtoken');
 const keycloakCerts = require('get-keycloak-public-key');
 const kc = new keycloakCerts('http://datasource-iam:8080', 'datasource');
+const fs = require('fs');
 
 server.use(middlewares)
+server.get('/metadata/:id/download', (req, res) => {
+    var resource = router.db.get('metadata')
+                     .find({ id: req.params.id })
+                     .value();
+
+    auth(req, res, resource.scope, function () {
+        fs.writeFile("metadata" + req.params.id, JSON.stringify(resource), function (err) {
+            if (err) return console.log(err);
+
+            res.download("metadata" + req.params.id);
+        });
+    });
+})
+
 server.use(router)
 
-router.render = async function (req, res) {
+auth = async function (req, res, scope, next) {
     const authHeader = req.headers.authorization;
-
-    if (Array.isArray(res.locals.data)) {
-        var scope = "list";
-        var resource = new hal.Resource({}, req.path);
-        var rows = [];
-
-        for (const row of res.locals.data) {
-            var embeddedResource = new hal.Resource(row, "/data/" + row.id);
-
-            rows.push(embeddedResource);
-        }
-
-        resource.embed("data", rows);
-    } else {
-        var scope = res.locals.data.type;
-
-        var resource = new hal.Resource(res.locals.data, req.path);
-    }
 
     if (authHeader) {
         const token = authHeader.split(' ')[1];
@@ -54,7 +51,7 @@ router.render = async function (req, res) {
 
                 req.user = user;
 
-                res.jsonp(resource);
+                next()
             });
         } else {
             return res.sendStatus(401);
@@ -62,6 +59,39 @@ router.render = async function (req, res) {
     } else {
         return res.sendStatus(401);
     }
+}
+
+router.render = async function (req, res) {
+    console.log(req.route);
+    console.log(req.path);
+
+    if (req.path == '/metadata') {
+        var scope = "metadata:read";
+        var resource = new hal.Resource({}, req.path);
+        var rows = [];
+
+        for (const row of res.locals.data) {
+            var embeddedResource = new hal.Resource(row, "/metadata/" + row.id);
+
+            embeddedResource.link('download', "/metadata/" + row.id + "/download");
+
+            rows.push(embeddedResource);
+        }
+
+        resource.embed("metadata", rows);
+    }
+
+    if (req.route.path == '/:id') {
+        var scope = res.locals.data.scope;
+
+        var resource = new hal.Resource(res.locals.data, req.path);
+
+        resource.link('download', "/metadata/" + res.locals.data.id + "/download");
+    }
+
+    auth(req, res, scope, function () {
+        res.jsonp(resource);
+    });
 }
 
 server.listen(3000, () => {
