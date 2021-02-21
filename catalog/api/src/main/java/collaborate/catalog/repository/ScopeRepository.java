e2ea9fa32e4b064c3518422841c50a1555cbfa2e
ThachThaoTran <ttran@theblockchainxdev.com>
2021-02-21 19:59:17 +0100

    COL-119 create an API endpoint to get scope by id

--- catalog/api/src/main/java/collaborate/catalog/repository/ScopeRepository.java
+++ catalog/api/src/main/java/collaborate/catalog/repository/ScopeRepository.java
@@ -3,7 +3,9 @@ package collaborate.catalog.repository;
 import collaborate.catalog.domain.Scope;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ScopeRepository {
     public List<Scope> findScopes();
+    public Scope findScopeById(UUID scopeId);
 }
--- catalog/api/src/main/java/collaborate/catalog/repository/ScopeRepositoryImpl.java
+++ catalog/api/src/main/java/collaborate/catalog/repository/ScopeRepositoryImpl.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.UUID;
 
 public class ScopeRepositoryImpl implements ScopeRepository {
 
@@ -27,5 +30,22 @@ public class ScopeRepositoryImpl implements ScopeRepository {
         AggregationResults<Scope> groupResults = mongoTemplate.aggregate(aggregation, "document", Scope.class);
 
         return groupResults.getMappedResults();
+    }
+
+    @Override
+    public Scope findScopeById(UUID scopeId) throws ResponseStatusException {
+        MatchOperation matchOperation = Aggregation.match(Criteria.where("type").is("metadata").and("scopeId").is(scopeId));
+
+        ProjectionOperation projectOperation = Aggregation.project("organizationId", "organizationName", "datasourceId", "scope", "scopeId");
+        GroupOperation groupOperation = Aggregation.group("organizationId", "organizationName", "datasourceId", "scope", "scopeId");
+
+        Aggregation aggregation = Aggregation.newAggregation(matchOperation, groupOperation, projectOperation);
+        AggregationResults<Scope> groupResults = mongoTemplate.aggregate(aggregation, "document", Scope.class);
+
+        if (groupResults.getMappedResults().isEmpty()) {
+            new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return groupResults.getMappedResults().get(0);
     }
 }
