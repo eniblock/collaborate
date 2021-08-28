347f05e886699735ddc1dee43d49a235c679dc4c
Sebastien Skorupski <sskorupski.ext@theblockchainxdev.com>
2021-08-28 22:14:51 +0200

    COL-274 Add create digital-passport postman test suite

--- dapp/api/src/main/java/collaborate/api/user/UserController.java
+++ dapp/api/src/main/java/collaborate/api/user/UserController.java
@@ -22,7 +22,6 @@ import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,7 +29,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -104,7 +102,6 @@ public class UserController {
           })
   })
   @PreAuthorize(HasRoles.PENDING_ASSET_OWNER)
-  @ResponseStatus(HttpStatus.CREATED)
   public Callable<ResponseEntity<UserDTO>> updateAsAssetOwner() {
     return () -> ResponseEntity.ok(userService.updateCurrentUserWithAssetOwnerRole());
   }
