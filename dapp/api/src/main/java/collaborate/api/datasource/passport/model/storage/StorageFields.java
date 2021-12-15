8d46b437867e5eabcb93ab8db81059b3ea7182c5
Jean-Rene Courtault <jrcourtault@theblockchainxdev.com>
2021-12-15 16:42:42 +0100

    new SC with indexers

--- dapp/api/src/main/java/collaborate/api/datasource/passport/model/storage/StorageFields.java
+++ dapp/api/src/main/java/collaborate/api/datasource/passport/model/storage/StorageFields.java
@@ -9,6 +9,6 @@ public class StorageFields {
   public static final String NFT_INDEXER = "nft_indexer";
   public static final String TOKEN_METADATA = "token_metadata";
   public static final String TOKEN_ID_BY_ASSET_ID = "token_id_by_asset_id";
-  public static final String TOKENS_BY_OWNER = "reverse_ledger";
+  public static final String TOKENS_BY_OWNER = "tokens_by_owner";
 
 }
